b5f930fdc6fa9024fb3da362b66f2d083b5eeaa9
Alexander Liesenfeld <alexander.liesenfeld@outlook.com>
2020-11-14 15:00:05 +0100

    Alpha version created

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,16 @@
 use async_std::sync::{Condvar, Mutex};
 
-pub struct Pool<T, F> where F: Fn() -> T {
+pub struct Pool<T> {
     sync_tuple: Mutex<(Vec<T>, usize)>,
     condvar: Condvar,
-    create_max: usize,
-    creator_fn: F,
+    create_max: usize
 }
 
-impl<T, F> Pool<T, F> where F: Fn() -> T {
-    pub fn new(create_max: usize, creator_fn: F) -> Self
-    where
-        F: Fn() -> T,
-    {
+impl<T> Pool<T> {
+    pub fn new(create_max: usize) -> Self {
         Self {
             sync_tuple: Mutex::new((Vec::new(), 0)),
             condvar: Condvar::new(),
-            creator_fn,
             create_max,
         }
     }
@@ -26,7 +21,8 @@ impl<T, F> Pool<T, F> where F: Fn() -> T {
         self.condvar.notify_one();
     }
 
-    pub async fn take(&self) -> T {
+    pub async fn take_or_create<F>(&self, creator_fn: F) -> T where
+        F: Fn() -> T {
         let mut lock_guard = (&self.sync_tuple).lock().await;
 
         while (*lock_guard).0.is_empty() && (*lock_guard).1 == self.create_max {
@@ -34,8 +30,7 @@ impl<T, F> Pool<T, F> where F: Fn() -> T {
         }
 
         if (*lock_guard).1 < self.create_max {
-            let elem = (self.creator_fn)();
-            (*lock_guard).0.push(elem);
+            (*lock_guard).0.push((creator_fn)());
             (*lock_guard).1 += 1;
         }
 
