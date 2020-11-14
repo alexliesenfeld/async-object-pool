18ae31979138db7a12a32b1c2d1c9358a163d646
Alexander Liesenfeld <alexander.liesenfeld@outlook.com>
2020-11-14 15:18:52 +0100

    Added documentation

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # puddle
 A simple object pool implementation that uses asynchronous synchronization primitives only. 
+
+You can use it as follows:
+
+```rust
+use async_object_pool::Pool;
+
+// Create a new pool that will allow to create at most 100 items
+let pool = Pool::new(100);
+
+// Take an item from the pool or create a new item if the pool is empty
+// but the maximum number of pooled items was not created yet.
+// This will asynchronously block execution until an item can be returned.
+let item = pool.take_or_create(|| String::from("hello")).await;
+
+// Use your item
+println!("{}", item);
+
+// After using the item, put it back into the pool so it can be reused elsewhere
+pool.put(item).await;
+```
+
+This crate is used to pool HTTP mock servers in [httpmock](https://github.com/alexliesenfeld/httpmock).
--- src/lib.rs
+++ src/lib.rs
@@ -37,3 +37,28 @@ impl<T> Pool<T> {
         return (*lock_guard).0.remove(0);
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use crate::Pool;
+
+    #[async_std::test]
+    async fn usage_example() -> std::io::Result<()> {
+        // Create a new pool that will allow to create at most 100 items
+        let pool = Pool::new(100);
+
+        // Take an item from the pool or create a new item if the pool is empty
+        // but the maximum number of pooled items was not created yet.
+        // This will asynchronously block execution until an item can be returned.
+        let item = pool.take_or_create(|| String::from("hello")).await;
+
+        // Use your item
+        println!("{}", item);
+
+        // After using the item, put it back into the pool so it can be reused elsewhere
+        pool.put(item).await;
+
+        Ok(())
+    }
+}
